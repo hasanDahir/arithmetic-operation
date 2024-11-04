fe3d4a03520a1129ce4d78083b178933d585b92c
hasanDahir <hassandahir096@gmail.com>
2024-11-04 10:51:46 -0600

    Exercise2

---
+++ Exercise2.js
@@ -0,0 +1,20 @@
+let a = 15;
+let b = "15";
+
+let equal = a == b;
+let strictEqual = a === b;
+let notEqual = a != b;
+let strictNotEqual = a !== b;
+let greaterThan = a > b;
+let lessThan = a < b;
+let greaterThanOrEqual = a >= b;
+let lessThanOrEqual = a <= b;
+
+console.log("Equal (==):", equal);
+console.log("Strict Equal (===):", strictEqual);
+console.log("Not Equal (!=):", notEqual);
+console.log("Strict Not Equal (!==):", strictNotEqual);
+console.log("Greater Than (>):", greaterThan);
+console.log("Less Than (<):", lessThan);
+console.log("Greater Than or Equal (>=):", greaterThanOrEqual);
+console.log("Less Than or Equal (<=):", lessThanOrEqual);
