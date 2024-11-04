be7d6a3af22a982ca6d4fef0ab8a41e79d0b98a9
hasanDahir <hassandahir096@gmail.com>
2024-11-04 10:17:32 -0600

    Exercise1

---
+++ exercise1.js
@@ -0,0 +1,18 @@
+let x = 7;
+let y = 3;
+
+let addition = x + y;
+let subtraction = x - y;
+let multiplication = x * y;
+let division = x / y;
+let floorDivision = Math.floor(x / y);
+let modulus = x % y;
+let exponentiation = x ** y;
+
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+console.log("Floor Division:", floorDivision);
+console.log("Modulus:", modulus);
+console.log("Exponentiation:", exponentiation);
