e805c0c87f0e9e0c7d9a3da97b54ef3ccfffa9b8
hasanDahir <hassandahir096@gmail.com>
2024-11-09 09:38:19 -0600

    Exercise4

---
+++ exercise4.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(2, 3));
+console.log(add(-1, 6));
+console.log(add(10, 15));
+
+
+const add = function(a, b) {
+    return a + b;
+};
+
+console.log(add(2, 3));
+console.log(add(-1, 6));
+console.log(add(10, 15));
